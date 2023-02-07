a412845b6e036bf5b31f0854081d3b5007655d97
fatoumaadel <111681952+fatoumaadel@users.noreply.github.com>
2023-02-07 13:50:41 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/apppliaction_with_amal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apppliaction_with_amal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  // ignore: prefer_const_constructors
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashScreen(),
+
+        };}}
